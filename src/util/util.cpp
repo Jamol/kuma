87236fba1faddcac9714a4b0bf0382b6d6dfd260
Jamol Bao <jamol@live.com>
2015-07-21 11:12:08 +0800

    initialize structs

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -306,8 +306,8 @@ extern "C" int km_get_sock_addr(struct sockaddr * sk_addr, unsigned int sk_addr_
 
 extern "C" bool km_is_ipv6_address(const char* addr)
 {
-    sockaddr_storage ss_addr;
-    struct addrinfo hints;
+    sockaddr_storage ss_addr = {0};
+    struct addrinfo hints = {0};
     hints.ai_family = AF_UNSPEC;
     hints.ai_flags = AI_NUMERICHOST;
     if(km_set_sock_addr(addr, 0, &hints, (struct sockaddr *)&ss_addr, sizeof(ss_addr)) != 0)
@@ -319,8 +319,8 @@ extern "C" bool km_is_ipv6_address(const char* addr)
 
 extern "C" bool km_is_ip_address(const char* addr)
 {
-    sockaddr_storage ss_addr;
-    struct addrinfo hints;
+    sockaddr_storage ss_addr = {0};
+    struct addrinfo hints = {0};
     hints.ai_family = AF_UNSPEC;
     hints.ai_flags = AI_NUMERICHOST;
     return km_set_sock_addr(addr, 0, &hints, (struct sockaddr *)&ss_addr, sizeof(ss_addr)) == 0;
@@ -328,8 +328,8 @@ extern "C" bool km_is_ip_address(const char* addr)
 
 extern "C" bool km_is_mcast_address(const char* addr)
 {
-    sockaddr_storage ss_addr;
-    struct addrinfo hints;
+    sockaddr_storage ss_addr = {0};
+    struct addrinfo hints = {0};
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_flags = AI_NUMERICHOST|AI_ADDRCONFIG; // will block 10 seconds in some case if not set AI_ADDRCONFIG
