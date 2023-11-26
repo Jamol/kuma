d50591bb3a4fe47964487e3225ff7c4043e4ab64
Jamol Bao <jamol@live.com>
2023-11-26 13:49:39 +0800

    add android test app

---
+++ test/android/kmtest/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMTester"
+include(":app")
+include(":kuma")
+project(":kuma").projectDir = file("../../../android/kuma/kuma")
